260678b4e293fc3286872ddeafceb651f48d5961
Bhuvnesh Sharma <164476865+bhuvnesh838@users.noreply.github.com>
2025-07-09 17:59:59 +0000

    ui update

---
+++ Hook/counter/counter/src/App.jsx
@@ -0,0 +1,30 @@
+ import react, { useState, useEffect} from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+ const[count, Counter] =useState (1)
+ ///let count=5;
+   const addValue =()=>{ 
+     Counter(count+1);
+  }  
+
+  const removeValue =()=>{
+    if(count<1) return;
+    Counter(count-1);
+  }
+  return (
+    <>
+      <h1>Vite Project</h1>
+      <h2>Counter value: {count}</h2>
+
+      <button onClick={addValue}>Add Value   </button> 
+      <br />
+      <br />
+      <button onClick={removeValue}>Remove Value</button>
+    </>
+  )
+}
+
+export default App
